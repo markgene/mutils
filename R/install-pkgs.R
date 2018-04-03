32289429899f6eee9d5cd50fce17f313fb59c5f4
Mark Chen <mjchen.gene@gmail.com>
2018-04-02 21:26:27 -0500

    feat: add install_pkgs()

--- R/install-pkgs.R
+++ R/install-pkgs.R
@@ -1,1 +1,27 @@
+# Install personal selected packages
+
+#' Install personal selected packages
+#'
+#' @return NULL
+install_pkgs <- function() {
+  cran_pkgs <-
+    c(
+      "devtools",
+      "dplyr",
+      "ggplot2",
+      "maps",
+      "roxygen2",
+      "rvest",
+      "stringr",
+      "testthat",
+      "tidyverse"
+    )
+  uninstalled <- cran_pkgs[!(cran_pkgs %in% installed.packages())]
+  if (length(uninstalled)) {
+    install.packages(uninstalled)
+    message("Success!")
+  } else {
+    message("All CRAN packages installed!")
+  }
+}
 
