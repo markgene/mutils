e8ede3a64ee7921f2c62976f91450c2497a11e11
Mark Chen <mjchen.gene@gmail.com>
2018-04-02 21:36:04 -0500

    feat: add install_pkgs_cran() and install_pkg_github()

--- R/install-pkgs.R
+++ R/install-pkgs.R
@@ -4,11 +4,20 @@
 #'
 #' @return NULL
 install_pkgs <- function() {
+#' Install personal selected packages on CRAN
+#'
+#' @return NULL
+install_pkgs_cran <- function() {
   cran_pkgs <-
     c(
       "devtools",
       "dplyr",
+      "fs",
+      "ggalt",
+      "ggraph",
       "ggplot2",
+      "hrbrthemes",
+      "igraph",
       "maps",
       "roxygen2",
       "rvest",
@@ -25,3 +34,18 @@ install_pkgs <- function() {
   }
 }
 
+
+#' Install personal selected packages on GitHub
+#'
+#' @return NULL
+install_pkgs_github <- function() {
+  gb_pkgs <-
+    c()
+  uninstalled <- gb_pkgs[!(gb_pkgs %in% installed.packages())]
+  if (length(uninstalled)) {
+    devtools::install_github(uninstalled)
+    message("Success!")
+  } else {
+    message("All GitHub packages installed!")
+  }
+}
