2af29b4e6e331c68e2f2e44dfe5e915d43279dd3
Mark J Chen <mjchen.gene@gmail.com>
2022-03-06 09:36:54 -0600

    feat: add stars_pval() function.

--- R/utils.R
+++ R/utils.R
@@ -47,3 +47,43 @@ split_path <- function(x) {
   else
     c(basename(x),split_path(dirname(x)))
 }
+
+
+#' Generate significance stars from p-values
+#'
+#' Generate significance stars (e.g. '***', '**', '*', 'ns') from p-values.
+#' Inspired by \code{gtools::stars.pval()} with extra arguments and flexibility
+#' and different default.
+#'
+#' Mapping from p-value ranges to symbols: \describe{ \item{0 - 0.001}{'***'}
+#' \item{0.001 - 0.01}{'**'} \item{0.01 - 0.05}{'*'}
+#' \item{0.05 - 1.0}{'ns'} }
+#'
+#' @param p.value numeric vector of p-values
+#' @param ... any parameters passed to \code{\link[stats]{symnum}}.
+#' @return A character vector containing the same number of elements as
+#' \code{p-value}, with an attribute "legend" providing the conversion pattern.
+#' @author Mark Chen \email{mjchen.gene@gmail.com}
+#' @seealso \code{\link[stats]{symnum}}
+#' @keywords misc
+#' @examples
+#'
+#' p.val <- c(0.0004, 0.0015, 0.013, 0.044, 0.067, 0.24)
+#' stars_pval(p.val)
+#' @importFrom stats symnum
+#' @export
+stars_pval <-
+  function(p.value,
+           cutpoints = c(0, 0.001, 0.01, 0.05, 1),
+           symbols = c("***", "**", "*", "ns"),
+           corr = FALSE,
+           na = FALSE,
+           ...) {
+    unclass(symnum(
+      p.value,
+      corr = corr,
+      na = na,
+      cutpoints = cutpoints,
+      symbols = symbols
+    ))
+  }
